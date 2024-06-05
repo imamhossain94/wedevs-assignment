ebc4ead2bd00482380eac40cb4212fdf98ceeea4
Md Imam Hossain <imamagun94@gmail.com>
2024-06-05 19:55:23 +0600

    native splash and launcher icon updated

--- lib/modules/home/views/home_page.dart
+++ lib/modules/home/views/home_page.dart
@@ -1,12 +1,41 @@
+import 'package:dokan/core/utils/dokan_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:dokan/modules/home/controllers/home_controller.dart';
+import 'package:sizer/sizer.dart';
 
 class HomePage extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(10.h),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          AppBar(
+            title: Text(
+              "Product List",
+              style: TextStyle(
+                  color: const Color(0xFF222455),
+                  fontSize: 18.sp,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.bold),
+            ),
+            centerTitle: true,
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            actions: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 15),
+                child: IconButton(
+                  icon: DokanIcon.icon(DokanIcon.search, size: 16.sp),
+                  onPressed: () {},
+                ),
+              )
+            ],
+          )
+        ]),
+      ),
       body: Obx(() {
         if (controller.products.isEmpty) {
           return const Center(child: CircularProgressIndicator());
